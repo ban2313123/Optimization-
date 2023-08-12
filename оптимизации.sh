55b37be447ad99ff4168082a927f6ed8bd5290fb
ban2313123 <128211950+ban2313123@users.noreply.github.com>
2023-08-13 06:47:12 +1000

    оптимизации.sh

--- оптимизации.sh
+++ оптимизации.sh
@@ -2,9 +2,9 @@
 echo просьба закрыть все програамы. для продолжения нажать энтер. ПОСЛЕ СИСТЕМА РЕБУТНИЦА. Не отходите от системы она может запрашивать пароль sudo
 read -s -p спасибо
 echo запуск обновление системы
-sudo pacman -Syuuu
+sudo pacman -Syuuu --noconfirm
 echo установка программ 
-sudo pacman -S  git make
+sudo pacman -S  --noconfirm git make
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -sric
@@ -26,7 +26,7 @@ echo установка софта для игр
 sudo pacman -S wine-staging winetricks wine-mono giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader lutris
 
 echo твики для игр
-sudo pacman -S gamemode lib32-gamemode
+sudo pacman -S --noconfirm gamemode lib32-gamemode
 
 echo оптимизация ssd
 sudo systemctl enable fstrim.timer            
@@ -49,7 +49,7 @@ cd ..
 sudo rf -rf ananicy
 cd
 sudo rm -rf Optimization 
-yay -Syuuu
-yay -S Octopi
+yay -Syuuu --noconfirm
+yay -S --noconfirm Octopi
 
 reboot
