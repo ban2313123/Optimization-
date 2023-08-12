88bf982076ae2ad304ecb2a17bebfcc6cbb7e722
ban2313123 <128211950+ban2313123@users.noreply.github.com>
2023-08-13 06:55:12 +1000

    оптимизации.sh

--- оптимизации.sh
+++ оптимизации.sh
@@ -22,8 +22,19 @@ sudo rm -rf auto-cpufreq
 cd
 
 
-echo установка софта для игр
+
+read -p "установить wine?Введите yes или no: " answer
+
+if [[ "$answer" == "yes" ]]; then
+    echo "Вы ввели yes"
+    echo начата установка wine
 sudo pacman -S wine-staging winetricks wine-mono giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader lutris
+"yes"
+elif [[ "$answer" == "no" ]]; then
+    echo хорошо скип
+    
+fi
+
 
 echo твики для игр
 sudo pacman -S --noconfirm gamemode lib32-gamemode
