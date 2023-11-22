32afdcf35d638eb07b2cde644dce118f24f1b544
ban2313123 <128211950+ban2313123@users.noreply.github.com>
2023-11-23 00:09:17 +1000

    Optimization-----.sh

--- Optimization-----.sh
+++ Optimization-----.sh
@@ -5,13 +5,13 @@ if [ $answer == *"y"* ]; then
 	sudo pacman -Syu --noconfirm git make --needed
 fi
 
-# git clone https://aur.archlinux.org/yay.git
-# cd yay
-# makepkg -sric
-# cd ..
-# sudo rm -rf yay
+ git clone https://aur.archlinux.org/yay.git
+ cd yay
+ makepkg -sric
+ cd ..
+sudo rm -rf yay
 
-read -p "Программа для оптимизация процессора [Y/n]" answer
+read -p "Программа для оптимизация процессора [y/n]" answer
 if [ $answer == *"n"* ]; then
 	echo "Скип"
 else
@@ -27,7 +27,7 @@ else
 fi
 
 read -p "Установить wine? [y/N]" answer
-if [ $answer == *"y"* ]; then
+if [ $answer == *"n"* ]; then
 	echo "Установка wine"
 	sudo pacman -S wine-staging winetricks wine-mono giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader lutris --needed
 else
@@ -37,7 +37,7 @@ fi
 echo "Твики для игр"
 sudo pacman -S --noconfirm --needed gamemode lib32-gamemode
 
-read -p "Оптимизация ssd [Y/n]" answer
+read -p "Оптимизация ssd [y/n]" answer
 if [[ $answer == *"n"* ]]; then
 	echo "Скип"
 else
@@ -46,7 +46,7 @@ else
 	sudo fstrim -va /
 fi
 
-read -p "Установить твики драйвера Nvidia? [Y/n]" answer
+read -p "Установить твики драйвера Nvidia? [y/n]" answer
 if [[ $answer == *"n"* ]]; then
 	echo "Скип"
 else
@@ -59,7 +59,7 @@ else
 	cd
 fi
 
-read -p "Ускоренный запуск [Y/n]" answer
+read -p "Ускоренный запуск [y/n]" answer
 if [[ $answer == *"n"* ]]; then
 	echo "Скип"
 else
@@ -75,7 +75,7 @@ fi
 # yay -Syuuu --noconfirm
 # yay -S --noconfirm Octopi
 
-read -p "Перезагрузка [Y/n]" answer
+read -p "Перезагрузка [y/n]" answer
 if [[ $answer == *"n"* ]]; then
 	echo "Перезагрузитесь"
 else
