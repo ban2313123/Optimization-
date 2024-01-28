d74dd0873a3ce7a6d76723722c077eb6cae3bdee
ban2313123 <z00022483@gmail.com>
2024-01-28 12:44:02 +1000

    Update Optimization.sh

--- Optimization.sh
+++ Optimization.sh
@@ -23,7 +23,7 @@ else
 	cd
 fi
 
-read -p "Установить wine? [y/N]" answer
+read -p "Установить wine? wine жто программа которая позволяет некоторые программы винловс запускать на линукс. [y/N]" answer
 if [ $answer == *"n"* ]; then
 	echo "Установка wine"
 	sudo pacman -S wine-staging winetricks wine-mono giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader  gamemode lib32-gamemode --needed
@@ -32,7 +32,7 @@ else
 fi
 
 
-read -p "Оптимизация ssd [y/n]" answer
+read -p "Оптимизация ssd не использовать если у вас hdd  [y/n]" answer
 if [[ $answer == *"n"* ]]; then
 	echo "Скип"
 else
