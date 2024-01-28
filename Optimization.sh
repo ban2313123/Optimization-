6c2ce30c43011495dc099f6f3ccceb76c3840d0b
ban2313123 <z00022483@gmail.com>
2024-01-28 12:41:44 +1000

    Update Optimization.sh

--- Optimization.sh
+++ Optimization.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
-read -sp "Можно закрыть все програмы. Для продолжения нажать Enter. После система перезагрузиться или вы сможете перезагрузить её позже сами. Не отходите от системы она может запрашивать пароль sudo и часто спрашивает подтверждение"
+read -sp " Не отходите от системы она может запрашивать пароль sudo "
 read -p "Обновить систему? [y/N]" answer
 if [ $answer == *"n"* ]; then
 	sudo pacman -Syu --noconfirm git make --needed
 fi
 mkdir cache
- git clone https://aur.archlinux.org/yay.git
- cd yay
- makepkg -sric
- cd ..
-sudo rm -rf yay
+
+
 
 read -p "Программа для оптимизация процессора [y/n]" answer
 if [ $answer == *"n"* ]; then
@@ -29,13 +26,11 @@ fi
 read -p "Установить wine? [y/N]" answer
 if [ $answer == *"n"* ]; then
 	echo "Установка wine"
-	sudo pacman -S wine-staging winetricks wine-mono giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader lutris --needed
+	sudo pacman -S wine-staging winetricks wine-mono giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader  gamemode lib32-gamemode --needed
 else
 	echo "Скип"
 fi
 
-echo "Твики для игр"
-sudo pacman -S --noconfirm --needed gamemode lib32-gamemode
 
 read -p "Оптимизация ssd [y/n]" answer
 if [[ $answer == *"n"* ]]; then
@@ -55,11 +50,11 @@ else
 	cd nvidia-tweaks
 	makepkg -sric
 	cd ..
-	rm -rf nvidia-tweaks
+	sudo rm -rf nvidia-tweaks
 	cd
 fi
 
-read -p "устоновка google-chrome [y/n]" answer
+read -p "устоновка google-chrome требуется yay [y/n]" answer
 if [[ $answer == *"n"* ]]; then
 	echo "Скип"
 else
@@ -94,7 +89,10 @@ fi
 
 read -p "Перезагрузка [y/n]" answer
 if [[ $answer == *"n"* ]]; then
-	echo "Перезагрузитесь"
+echo "шутите? АНУ БЫСТРО НА РЕБУт"
+    sleep 4
+	reboot 
+	
 else
 	reboot
 fi
